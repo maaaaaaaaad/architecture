8f7dd20b34ac53c8fdc40a6243dac9d68709a098
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-25 07:39:26 +0000

    e

--- main.go
+++ main.go
@@ -2,27 +2,30 @@ package main
 
 import "fmt"
 
-func binarySearch(arr []int, low, high, target int) int {
-	if low > high {
-		return -1
+func binarySearch(arr []int, target int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
 	}
 
-	mid := (low + high) / 2
-
-	if arr[mid] == target {
-		return mid
-	} else if arr[mid] < target {
-		return binarySearch(arr, mid+1, high, target)
-	} else {
-		return binarySearch(arr, low, mid-1, target)
-	}
+	return -1
 }
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	target := 6
 
-	index := binarySearch(arr, 0, len(arr)-1, target)
+	index := binarySearch(arr, target)
 
 	if index != -1 {
 		fmt.Printf("Target %d found at index %d\n", target, index)
