0243419396a5f37ba0be647cc89a93a6dc4271e9
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-06 10:24:16 +0000

    e

--- main.go
+++ main.go
@@ -1,58 +1,63 @@
-// package main
-
-// import "fmt"
-
-// func binarySearch(arr []int, target int) int {
-// 	low := 0
-// 	high := len(arr) - 1
-
-// 	for low <= high {
-// 		mid := (low + high) / 2
-
-// 		if arr[mid] == target {
-// 			return mid
-// 		} else if arr[mid] < target {
-// 			low = mid + 1
-// 		} else {
-// 			high = mid - 1
-// 		}
-// 	}
-
-// 	return -1
-// }
-
-// func main() {
-// 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-// 	target := 6
-
-// 	index := binarySearch(arr, target)
-
-// 	if index != -1 {
-// 		fmt.Printf("Target %d found at index %d\n", target, index)
-// 	} else {
-// 		fmt.Printf("Target %d not found in the array\n", target)
-// 	}
-// }
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func findMax(arr []int) int {
-	max := arr[0]
+type Graph struct {
+	vertices []*Vertex
+}
 
-	for i := 1; i < len(arr); i++ {
-		if arr[i] > max {
-			max = arr[i]
+type Vertex struct {
+	key      int
+	adjacent []*Vertex
+	visited  bool
+}
+
+func NewVertex(key int) *Vertex {
+	return &Vertex{
+		key:      key,
+		adjacent: []*Vertex{},
+	}
+}
+
+func (g *Graph) AddVertex(v *Vertex) {
+	g.vertices = append(g.vertices, v)
+}
+
+func (g *Graph) AddEdge(v1, v2 *Vertex) {
+	v1.adjacent = append(v1.adjacent, v2)
+	v2.adjacent = append(v2.adjacent, v1)
+}
+
+func (g *Graph) DFS(v *Vertex) {
+	v.visited = true
+	fmt.Printf("Visited vertex: %d\n", v.key)
+
+	for _, adjacent := range v.adjacent {
+		if !adjacent.visited {
+			g.DFS(adjacent)
 		}
 	}
-
-	return max
 }
 
 func main() {
-	arr := []int{10, 20, 30, 50, 40, 60, 70, 80, 90}
-	max := findMax(arr)
+	g := &Graph{}
 
-	fmt.Printf("Maximum value in the array is %d\n", max)
+	vertices := []*Vertex{}
+	for i := 0; i < 8; i++ {
+		vertex := NewVertex(i)
+		vertices = append(vertices, vertex)
+		g.AddVertex(vertex)
+	}
+
+	g.AddEdge(vertices[0], vertices[1])
+	g.AddEdge(vertices[0], vertices[2])
+	g.AddEdge(vertices[1], vertices[3])
+	g.AddEdge(vertices[1], vertices[4])
+	g.AddEdge(vertices[2], vertices[5])
+	g.AddEdge(vertices[2], vertices[6])
+	g.AddEdge(vertices[3], vertices[7])
+
+	g.DFS(vertices[0])
 }
