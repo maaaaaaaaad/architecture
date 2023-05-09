7aeadbffd2bc0e8c6d0beb63fc8e6389f8a9d41e
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-09 04:45:03 +0000

    ee

--- main.go
+++ main.go
@@ -2,62 +2,42 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-type Graph struct {
-	vertices []*Vertex
-}
-
-type Vertex struct {
-	key      int
-	adjacent []*Vertex
-	visited  bool
-}
-
-func NewVertex(key int) *Vertex {
-	return &Vertex{
-		key:      key,
-		adjacent: []*Vertex{},
-	}
-}
-
-func (g *Graph) AddVertex(v *Vertex) {
-	g.vertices = append(g.vertices, v)
-}
-
-func (g *Graph) AddEdge(v1, v2 *Vertex) {
-	v1.adjacent = append(v1.adjacent, v2)
-	v2.adjacent = append(v2.adjacent, v1)
-}
-
-func (g *Graph) DFS(v *Vertex) {
-	v.visited = true
-	fmt.Printf("Visited vertex: %d\n", v.key)
-
-	for _, adjacent := range v.adjacent {
-		if !adjacent.visited {
-			g.DFS(adjacent)
+func solution(distance int, rocks []int, n int) int {
+	sort.Ints(rocks)
+	left, right := 1, distance
+	var mid int
+	for left <= right {
+		mid = (left + right) / 2
+		removed := 0
+		prev := 0
+		for _, rock := range rocks {
+			if rock-prev < mid {
+				removed++
+			} else {
+				prev = rock
+			}
+		}
+		if distance-prev < mid {
+			removed++
+		}
+		if removed > n {
+			right = mid - 1
+		} else {
+			left = mid + 1
 		}
 	}
+
+	return right
 }
 
 func main() {
-	g := &Graph{}
+	distance := 25
+	rocks := []int{2, 14, 11, 21, 17}
+	n := 2
+	result := solution(distance, rocks, n)
+	fmt.Println(result)
 
-	vertices := []*Vertex{}
-	for i := 0; i < 8; i++ {
-		vertex := NewVertex(i)
-		vertices = append(vertices, vertex)
-		g.AddVertex(vertex)
-	}
-
-	g.AddEdge(vertices[0], vertices[1])
-	g.AddEdge(vertices[0], vertices[2])
-	g.AddEdge(vertices[1], vertices[3])
-	g.AddEdge(vertices[1], vertices[4])
-	g.AddEdge(vertices[2], vertices[5])
-	g.AddEdge(vertices[2], vertices[6])
-	g.AddEdge(vertices[3], vertices[7])
-
-	g.DFS(vertices[0])
 }
